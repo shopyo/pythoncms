96eac100d7fafbed01a4dc30ae03504616d6f0c4
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-03-20 13:44:36 +0400

    init

---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+Welcome to src docs!
+============================
+
+Write project description here
+
+.. toctree::
+   :maxdepth: 9
+
+   Docs <docs>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ src/init.py
@@ -0,0 +1,23 @@
+"""
+All initializations like db = SQLAlchemy in this file
+"""
+import os
+
+from flask_login import LoginManager
+from flask_mailman import Mail
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+root_path = os.path.dirname(os.path.abspath(__file__))  # don't remove
+static_path = os.path.join(root_path, "static")  # don't remove
+modules_path = os.path.join(root_path, "modules")  # don't remove
+themes_path = os.path.join(static_path, "themes")  # don't remove
+
+db = SQLAlchemy()
+ma = Marshmallow()
+login_manager = LoginManager()
+migrate = Migrate()
+mail = Mail()
+csrf = CSRFProtect()
